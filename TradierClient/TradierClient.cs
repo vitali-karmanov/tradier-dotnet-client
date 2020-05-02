1fa21eeae0d1991b4ed70cb23b83e8689f5d8546
vitali-karmanov <40288109+vitali-karmanov@users.noreply.github.com>
2020-05-02 17:34:07 -0600

    Added HttpClientFactory for sandbox with basic getUserProfile() and getBalances() calls

---
+++ TradierClient/TradierClient.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Http;
+
+namespace TradierClient
+{
+    public class TradierClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public TradierClient(string accessToken)
+        {
+            HttpClientFactory httpClientFactory = new HttpClientFactory();
+            httpClientFactory.Register("sandbox", builder => builder
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://sandbox.tradier.com/v1/"))
+            .ConfigureHttpClient(c => c.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken))
+            .ConfigureHttpClient(c => c.DefaultRequestHeaders.Add("Accept", "application/json")));
+
+            _httpClient = httpClientFactory.CreateClient("sandbox");
+
+        }
+
+        public async Task<string> getUserProfile()
+        {
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "user/profile"))
+            using (var response = await _httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+
+        }
+
+        public async Task<string> getBalances(string accountNumber)
+        {
+            using (var request = new HttpRequestMessage(HttpMethod.Get, "accounts/" + accountNumber + "/balances"))
+            using (var response = await _httpClient.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+
+        }
+    }
+}
