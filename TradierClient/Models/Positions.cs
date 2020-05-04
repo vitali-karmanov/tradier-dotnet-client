a7bb01aeb94d56665c320a17dcd3a23c68bc954e
Henrique Tedeschi <tedeschi@byui.edu>
2020-05-04 00:17:21 -0600

    Adding positions model and getpositions request.

---
+++ TradierClient/Models/Positions.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using Tradier.Client.Helpers;
+
+namespace Tradier.Client.Models
+{
+    public class PositionsRootobject
+    {
+        [JsonProperty("positions")] 
+        public Positions? Positions { get; set; }
+    }
+
+    public class Positions
+    {
+        [JsonProperty("position")]
+        [JsonConverter(typeof(SingleOrArrayConverter<Position>))]
+        public List<Position>? Position { get; set; }
+    }
+
+    public class Position
+    {
+        [JsonProperty("cost_basis")]
+        public float CostBasis { get; set; }
+
+        [JsonProperty("date_acquired")]
+        public DateTime DateAcquired { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("quantity")]
+        public float Quantity { get; set; }
+
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; }
+    }
+}
--- TradierClient/TradierClient/TradierClient.Account.cs
+++ TradierClient/TradierClient/TradierClient.Account.cs
@@ -1,35 +1,47 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Tradier.Client.Models;
 
+// ReSharper disable once CheckNamespace
 namespace Tradier.Client
 {
     public sealed partial class TradierClient
     {
         public async Task<Profile> GetUserProfile()
         {
-            using (var request = new HttpRequestMessage(HttpMethod.Get, "user/profile"))
-            using (var response = await _httpClient.SendAsync(request))
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
+            using var request = new HttpRequestMessage(HttpMethod.Get, "user/profile");
+            using var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            content = content.Replace("\"null\"", "null");
 
-                return JsonConvert.DeserializeObject<ProfileRootObject>(content).Profile;
-            }
+            return JsonConvert.DeserializeObject<ProfileRootObject>(content).Profile;
         }
 
         public async Task<Balances> GetBalances(string accountNumber)
         {
-            using (var request = new HttpRequestMessage(HttpMethod.Get, "accounts/" + accountNumber + "/balances"))
-            using (var response = await _httpClient.SendAsync(request))
+            using var request = new HttpRequestMessage(HttpMethod.Get, "accounts/" + accountNumber + "/balances");
+            using var response = await _httpClient.SendAsync(request);
             {
                 response.EnsureSuccessStatusCode();
                 var content = await response.Content.ReadAsStringAsync();
+                content = content.Replace("\"null\"", "null");
 
                 return JsonConvert.DeserializeObject<BalanceRootObject>(content).Balances;
+            }
+        }
+
+        public async Task<Positions> GetPositions(string accountNumber)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, "accounts/" + accountNumber + "/positions");
+            using var response = await _httpClient.SendAsync(request);
+            {
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                content = content.Replace("\"null\"", "null");
+
+                return JsonConvert.DeserializeObject<PositionsRootobject>(content).Positions;
             }
         }
     }
