76283e02a130c09041e8a143fe7643e7ae21a696
Henrique Tedeschi <tedeschi@byui.edu>
2020-05-05 22:21:39 -0600

    Removing repeated code, centralizing requests in a helper

---
+++ TradierClient/Helpers/Requests.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Tradier.Client.Helpers
+{
+    public class Requests<T>
+    {
+        private readonly HttpClient _httpClient;
+
+        public Requests(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> GetDeserialized(Uri uri)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            using var response = await _httpClient.SendAsync(request);
+            {
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                content = content.Replace("\"null\"", "null");
+
+                return JsonConvert.DeserializeObject<T>(content);
+            }
+        }
+
+        public async Task<T> GetDeserialized(string uri)
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            using var response = await _httpClient.SendAsync(request);
+            {
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                content = content.Replace("\"null\"", "null");
+
+                return JsonConvert.DeserializeObject<T>(content);
+            }
+        }
+    }
+}
--- TradierClient/TradierClient/TradierClient.Account.cs
+++ TradierClient/TradierClient/TradierClient.Account.cs
@@ -1,7 +1,8 @@
-﻿using System.Linq;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Tradier.Client.Helpers;
 using Tradier.Client.Models;
 
 // ReSharper disable once CheckNamespace
@@ -11,91 +12,44 @@ namespace Tradier.Client
     {
         public async Task<Profile> GetUserProfile()
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, "user/profile");
-            using var response = await _httpClient.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            var content = await response.Content.ReadAsStringAsync();
-            content = content.Replace("\"null\"", "null");
-
-            return JsonConvert.DeserializeObject<ProfileRootObject>(content).Profile;
+            Requests<ProfileRootObject> request = new Requests<ProfileRootObject>(_httpClient);
+            return (await request.GetDeserialized("user/profile")).Profile;
         }
 
         public async Task<Balances> GetBalances(string accountNumber)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, "accounts/" + accountNumber + "/balances");
-            using var response = await _httpClient.SendAsync(request);
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                content = content.Replace("\"null\"", "null");
-
-                return JsonConvert.DeserializeObject<BalanceRootObject>(content).Balances;
-            }
+            Requests<BalanceRootObject> request = new Requests<BalanceRootObject>(_httpClient);
+            return (await request.GetDeserialized($"accounts/{accountNumber}/balances")).Balances;
         }
 
         public async Task<Positions> GetPositions(string accountNumber)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, "accounts/" + accountNumber + "/positions");
-            using var response = await _httpClient.SendAsync(request);
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                content = content.Replace("\"null\"", "null");
-
-                return JsonConvert.DeserializeObject<PositionsRootobject>(content).Positions;
-            }
+            Requests<PositionsRootobject> request = new Requests<PositionsRootobject>(_httpClient);
+            return (await request.GetDeserialized($"accounts/{accountNumber}/positions")).Positions;
         }
 
         public async Task<History> GetHistory(string accountNumber, int page = 1, int limitPerPage = 25)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, $"accounts/{accountNumber}/history?page={page}&limit={limitPerPage}");
-            using var response = await _httpClient.SendAsync(request);
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                content = content.Replace("\"null\"", "null");
-
-                return JsonConvert.DeserializeObject<HistoryRootobject>(content).History;
-            }
+            Requests<HistoryRootobject> request = new Requests<HistoryRootobject>(_httpClient);
+            return (await request.GetDeserialized($"accounts/{accountNumber}/history?page={page}&limit={limitPerPage}")).History;
         }
 
         public async Task<GainLoss> GetGainLoss(string accountNumber, int page = 1, int limitPerPage = 25)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, $"accounts/{accountNumber}/gainloss?page={page}&limit={limitPerPage}");
-            using var response = await _httpClient.SendAsync(request);
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                content = content.Replace("\"null\"", "null");
-
-                return JsonConvert.DeserializeObject<GainLossRootobject>(content).GainLoss;
-            }
+            Requests<GainLossRootobject> request = new Requests<GainLossRootobject>(_httpClient);
+            return (await request.GetDeserialized($"accounts/{accountNumber}/gainloss?page={page}&limit={limitPerPage}")).GainLoss;
         }
 
         public async Task<Orders> GetOrders(string accountNumber)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, $"accounts/{accountNumber}/orders");
-            using var response = await _httpClient.SendAsync(request);
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                content = content.Replace("\"null\"", "null");
-
-                return JsonConvert.DeserializeObject<OrdersRootobject>(content).Orders;
-            }
+            Requests<OrdersRootobject> request = new Requests<OrdersRootobject>(_httpClient);
+            return (await request.GetDeserialized($"accounts/{accountNumber}/orders")).Orders;
         }
 
         public async Task<Order> GetOrder(string accountNumber, string orderId)
         {
-            using var request = new HttpRequestMessage(HttpMethod.Get, $"accounts/{accountNumber}/orders/{orderId}");
-            using var response = await _httpClient.SendAsync(request);
-            {
-                response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                content = content.Replace("\"null\"", "null");
-
-                return JsonConvert.DeserializeObject<Orders>(content).Order.FirstOrDefault();
-            }
+            Requests<Orders> request = new Requests<Orders>(_httpClient);
+            return (await request.GetDeserialized($"accounts/{accountNumber}/orders/{orderId}")).Order.FirstOrDefault();
         }
     }
 }
