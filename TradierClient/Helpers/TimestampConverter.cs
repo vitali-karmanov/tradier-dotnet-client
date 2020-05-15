87654fab1b82a7257d47fd25f55123c48fa4b7f1
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-15 15:55:46 -0600

    Create a Timestamp converter to convert UNIX time to DateTime

---
+++ TradierClient/Helpers/TimestampConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Tradier.Client.Helpers
+{
+    public class TimestampConverter : Newtonsoft.Json.JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(DateTime);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var tt = reader.Value.ToString();
+            var t = long.Parse(tt);
+            return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(t).ToLocalTime();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
