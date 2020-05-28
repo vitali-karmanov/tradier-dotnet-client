d4b403ac2a1cf665c1a244dad997d0434d6abaa3
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-27 21:05:46 -0600

    Remove unnecessary usings

--- TradierClient/TradierClient/Watchlist.cs
+++ TradierClient/TradierClient/Watchlist.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Tradier.Client.Helpers;
-using Tradier.Client.Models.MarketData;
 using Tradier.Client.Models.Watchlist;
 
 namespace Tradier.Client
