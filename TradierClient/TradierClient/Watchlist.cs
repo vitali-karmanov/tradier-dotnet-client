59d74f0543d3ac35d5a9be7426e57969b41359b8
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-05-27 21:04:57 -0600

    Created CreateWatchlist and its overload in Watchlist class

--- TradierClient/TradierClient/Watchlist.cs
+++ TradierClient/TradierClient/Watchlist.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Tradier.Client.Helpers;
+using Tradier.Client.Models.MarketData;
 using Tradier.Client.Models.Watchlist;
 
 namespace Tradier.Client
@@ -23,6 +27,25 @@ namespace Tradier.Client
         public async Task<Watchlists> GetWatchlist(string watchlistId)
         {
             var response = await _requests.GetRequest($"watchlists/{watchlistId}");
+            return JsonConvert.DeserializeObject<Watchlists>(response);
+        }
+
+        public async Task<Watchlists> CreateWatchlist(string name, string symbols)
+        {
+            List<string> listSymbols = symbols.Split(',').Select(x => x.Trim()).ToList();
+            return await CreateWatchlist(name, listSymbols);
+        }
+
+        public async Task<Watchlists> CreateWatchlist(string name, List<string> symbols)
+        {
+            string strSymbols = String.Join(",", symbols);
+            var data = new Dictionary<string, string>
+            {
+                { "name", name },
+                { "symbols", strSymbols },
+            };
+
+            var response = await _requests.PostRequest($"watchlists", data);
             return JsonConvert.DeserializeObject<Watchlists>(response);
         }
     }
