85907c4fa4940dc5312026218ae2474384c94543
vitali-karmanov <40288109+vitali-karmanov@users.noreply.github.com>
2020-06-01 22:41:44 -0600

    Added option to pass your own HttpClient

--- TradierClient/TradierClient/TradierClient.cs
+++ TradierClient/TradierClient/TradierClient.cs
@@ -8,21 +8,25 @@ namespace Tradier.Client
 {
     public class TradierClient
     {
-        private readonly HttpClient _httpClient = new HttpClient();
         public Account Account { get; set; }
         public MarketData MarketData { get; set; }
         public Trading Trading { get; set; }
         public Watchlist Watchlist { get; set; }
 
         public TradierClient(string apiToken, bool useProduction = false)
+           : this(new HttpClient(), apiToken, useProduction)
+        {
+        }
+
+        public TradierClient(HttpClient httpClient, string apiToken, bool useProduction = false)
         {
             Uri baseEndpoint = useProduction ? new Uri(Settings.PRODUCTION_ENDPOINT) : new Uri(Settings.SANDBOX_ENDPOINT);
 
-            _httpClient.BaseAddress = baseEndpoint;
-            _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiToken}");
-            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            httpClient.BaseAddress = baseEndpoint;
+            httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiToken}");
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
 
-            Requests request = new Requests(_httpClient);
+            Requests request = new Requests(httpClient);
 
             Account = new Account(request);
             MarketData = new MarketData(request);
