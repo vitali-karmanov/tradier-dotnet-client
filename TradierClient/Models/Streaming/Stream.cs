7f8d045969935697a2fc41a9f59c00142c634859
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-06-02 21:29:26 -0600

    Create Stream model and Streaming class with CreateMarketSession method

---
+++ TradierClient/Models/Streaming/Stream.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Tradier.Client.Models.Streaming
+{
+
+    public class StreamRootobject
+    {
+        [JsonProperty("stream")]
+        public Stream Stream { get; set; }
+    }
+
+    public class Stream
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        
+        [JsonProperty("sessionid")]
+        public string SessionId { get; set; }
+    }
+
+}
---
+++ TradierClient/TradierClient/Streaming.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tradier.Client.Helpers;
+using Tradier.Client.Models.Streaming;
+using Tradier.Client.Models.Trading;
+
+namespace Tradier.Client
+{
+    public class Streaming
+    {
+        private readonly Requests _requests;
+
+        public Streaming(Requests requests)
+        {
+            _requests = requests;
+        }
+
+        public async Task<Stream> CreateMarketSession()
+        {
+            var response = await _requests.PostRequest($"markets/events/session");
+            return JsonConvert.DeserializeObject<StreamRootobject>(response).Stream;
+        }
+    }
+}
