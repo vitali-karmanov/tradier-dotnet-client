c498ee8e4acb5d0182a12ba019173cb81c992363
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-06-10 21:45:22 -0600

    In-code documentation for Authentication

--- TradierClient/TradierClient/Authentication.cs
+++ TradierClient/TradierClient/Authentication.cs
@@ -7,15 +7,24 @@ using Tradier.Client.Models.Authentication;
 // ReSharper disable once CheckNamespace
 namespace Tradier.Client
 {
+    /// <summary>
+    /// The <c>Authentication</c> class
+    /// </summary>
     public class Authentication
     {
         private readonly Requests _requests;
-
+        
+        /// <summary>
+        /// The Authentication constructor
+        /// </summary>
         public Authentication(Requests requests)
         {
             _requests = requests;
         }
 
+        /// <summary>
+        /// You can obtain an access token by exchanging an authorization code
+        /// </summary>
         public async Task<Token> CreateAccessToken(string code)
         {
             var data = new Dictionary<string, string>
@@ -28,6 +37,9 @@ namespace Tradier.Client
             return JsonConvert.DeserializeObject<Token>(response);
         }
 
+        /// <summary>
+        /// You will obtain a refresh token in the same response as an access token
+        /// </summary>
         public async Task<Token> RefreshAccessToken(string refreshToken)
         {
             var data = new Dictionary<string, string>
