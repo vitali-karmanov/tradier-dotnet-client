f8f61d0001763686540712a12a962cabc62dfda6
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-06-28 22:46:04 -0600

    Changed the exception from MissingDefaultAccountNumberException to MissingAccountNumberException

---
+++ TradierClient/Exceptions/MissingAccountNumberException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tradier.Client.Exceptions
+{
+    public class MissingAccountNumberException : Exception
+    {
+        public MissingAccountNumberException()
+        {
+        }
+
+        public MissingAccountNumberException(string message)
+            : base(message)
+        {
+        }
+
+        public MissingAccountNumberException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
