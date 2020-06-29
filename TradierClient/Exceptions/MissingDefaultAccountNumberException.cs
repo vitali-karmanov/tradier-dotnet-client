277406990c4e4c67c6b769b34b754f839732ba61
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-06-28 22:44:45 -0600

    Created user-defined exception MissingDefaultAccountNumberException

---
+++ TradierClient/Exceptions/MissingDefaultAccountNumberException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tradier.Client.Exceptions
+{
+    public class MissingDefaultAccountNumberException : Exception
+    {
+        public MissingDefaultAccountNumberException()
+        {
+        }
+
+        public MissingDefaultAccountNumberException(string message)
+            : base(message)
+        {
+        }
+
+        public MissingDefaultAccountNumberException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
