25d97ce5dff1d76695704c9ce428731b70bb2214
Henrique Tedeschi <henrique.tedeschi@gmail.com>
2020-08-03 15:59:48 -0600

    Added TradierClientException and made it so the messages now appear in the exception form

---
+++ TradierClient/Exceptions/TradierClientException.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Tradier.Client.Models.Exception;
+
+namespace Tradier.Client.Exceptions
+{
+    public class TradierClientException : Exception
+    {
+        public TradierClientException()
+        {
+        }
+
+        public TradierClientException(string message)
+        : base(message)
+        {
+        }
+
+        public TradierClientException(HttpResponseMessage response)
+        {
+            Task<string> resp = response.Content.ReadAsStringAsync();
+            if (!string.IsNullOrEmpty(resp.Result))
+            {
+                if (Equals(response.Content.Headers.ContentType, MediaTypeHeaderValue.Parse("application/json")))
+                {
+                    Fault fault = JsonConvert.DeserializeObject<FaultRootobject>(resp.Result).Fault;
+                    throw new TradierClientException(fault, response);
+
+                }
+
+                throw new TradierClientException(resp.Result);
+            }
+
+            // if it's not any of the cases above,
+            // just output the http status code exception embedded in EnsureSuccessStatusCode()
+            response.EnsureSuccessStatusCode();
+        }
+
+        public TradierClientException(Fault fault, HttpResponseMessage response)
+        {
+            throw new Exception(responseBuilder(response, fault));
+        }
+
+        public TradierClientException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+
+        private string responseBuilder(HttpResponseMessage response, Fault fault = null)
+        {
+            var messageStream = response.Content.ReadAsStreamAsync().Result;
+            var messageField = (fault != null ? fault.FaultString : messageStream.ToString());
+            string messageBuilt = $"IsSuccessStatusCode: {response.IsSuccessStatusCode}\n" +
+                             $"Reason: {response.ReasonPhrase}\n" +
+                             $"StatusCode: {response.StatusCode.ToString()}\n" +
+                             $"Message: {messageField}";
+
+            return messageBuilt;
+        }
+    }
+}
---
+++ TradierClient/Models/Exception/Fault.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Tradier.Client.Models.Exception
+{
+    public class FaultRootobject
+    {
+        [JsonProperty("fault")]
+        public Fault Fault { get; set; }
+    }
+
+    public class Fault
+    {
+        [JsonProperty("faultstring")]
+        public string FaultString { get; set; }
+        [JsonProperty("detail")]
+        public Detail Detail { get; set; }
+    }
+
+    public class Detail
+    {
+        [JsonProperty("errorcode")]
+        public string ErrorCode { get; set; }
+    }
+
+}
