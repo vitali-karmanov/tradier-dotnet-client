92e3a2ce5177689cc3ee3dd7cdaa16d3111dab44
longpshorn <patrick.j.smith1@gmail.com>
2021-04-15 05:04:21 -0500

    Updating the StreamResponse model to include PascalCased property names. Removing duplicate test class.

--- TradierClient.Test/Tests/StreamingTests.cs
+++ TradierClient.Test/Tests/StreamingTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using NUnit.Framework;
-using Tradier.Client;
 using Tradier.Client.Models.Streaming;
 using TradierClient.Test.Helpers;
 
